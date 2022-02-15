4b9678b9f44425d722a6fe9e74190a355fd2db9f
Dan Sosedoff <dan.sosedoff@gmail.com>
2022-02-15 12:30:04 -0600

    Add server index and a status endpoint

--- core/server.go
+++ core/server.go
@@ -8,6 +8,24 @@ import (
 	"github.com/streamingfast/dummy-blockchain/types"
 )
 
+const (
+	homePage = `
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+<div class="container">
+	<h1>Dummy Blockchain</h1>
+	<p>You're looking at the Dummy block chain server implementation!</p>
+	<hr/>
+	<h2>Routes</h3>
+	<ul>
+		<li><code>/</code> - View current page</li>
+		<li><code>/status</code> - Chain status</li>
+		<li><code>/block</code> - Current block</li>
+		<li><code>/blocks/:height</code> - Get block by height</li>
+	</ul>
+</div>
+	`
+)
+
 type Server struct {
 	*gin.Engine
 
@@ -26,6 +44,8 @@ func NewServer(store *Store, addr string) Server {
 		addr:   addr,
 	}
 
+	server.GET("/", server.getHome)
+	server.GET("/status", server.getStatus)
 	server.GET("/block", server.getBlock)
 	server.GET("/blocks/:id", server.getBlock)
 
@@ -40,6 +60,15 @@ func (s *Server) Start() error {
 	}
 
 	return err
+}
+
+func (s *Server) getHome(c *gin.Context) {
+	c.Header("Content-Type", "text/html")
+	c.String(200, homePage)
+}
+
+func (s *Server) getStatus(c *gin.Context) {
+	c.JSON(200, s.store.meta)
 }
 
 func (s *Server) getBlock(c *gin.Context) {
