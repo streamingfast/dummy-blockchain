995a14e3eb54632005462ecdb224a2aff4d37058
Matthieu Vachon <matt@streamingfast.io>
2025-07-23 15:00:55 -0400

    Fixed missing move of dummy-blockchain

---
+++ cmd/dummy-blockchain/version.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+var (
+	Version = "0.0.1"
+)
+
+func VersionString() string {
+	gitCommit := "unknown"
+	gitDate := "unknown"
+
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			switch setting.Key {
+			case "vcs.revision":
+				gitCommit = setting.Value
+			case "vcs.time":
+				gitDate = setting.Value
+			}
+		}
+	}
+
+	return fmt.Sprintf(
+		"%v (Commit=%q Commit Date=%q)",
+		Version, gitCommit, gitDate,
+	)
+}
